046422508568b2fa0bb88ded7bf197aede79fc10
ngthloi <115602142+ngthloi@users.noreply.github.com>
2023-06-04 16:14:54 +0700

    Delete NguyenThangLoii directory

--- NguyenThangLoii/src/main/java/com/NguyenThangLoi/repository/NhanVienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.NguyenThangLoi.repository;
-
-import com.NguyenThangLoi.model.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
-}
