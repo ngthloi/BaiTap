a8c0a83b2354bd6589a84b09f343b80bde00ae49
ngthloi <115602142+ngthloi@users.noreply.github.com>
2023-06-04 12:25:42 +0700

    Delete vochiben directory

--- vochiben/src/main/java/vochiben/repository/NhanVienRepository.java
+++
@@ -1,7 +0,0 @@
-package vochiben.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vochiben.model.NhanVien;
-
-public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
-}
--- vochiben/src/main/java/vochiben/repository/PhongBanRepository.java
+++
@@ -1,8 +0,0 @@
-package vochiben.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vochiben.model.PhongBan;
-
-public interface PhongBanRepository extends JpaRepository<PhongBan, String> {
-}
