99b1f020a016305e0cc354bfeaf37a94de3331b1
ngthloi <115602142+ngthloi@users.noreply.github.com>
2023-06-04 12:31:14 +0700

    Add files via upload

---
+++ NguyenThangLoii/src/main/java/com/NguyenThangLoi/repository/NhanVienRepository.java
@@ -0,0 +1,7 @@
+package com.NguyenThangLoi.repository;
+
+import com.NguyenThangLoi.model.NhanVien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
+}
---
+++ NguyenThangLoii/src/main/java/com/NguyenThangLoi/repository/PhongBanRepository.java
@@ -0,0 +1,8 @@
+package com.NguyenThangLoi.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.NguyenThangLoi.model.PhongBan;
+
+public interface PhongBanRepository extends JpaRepository<PhongBan, String> {
+}
